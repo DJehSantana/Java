b8d2bf6da6a0aac6244db0c1e51221b670d74d30
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-13 17:25:38 -0300

    exercício de FOR, tabuada

--- Main/Aula_JavaRepeticoes.java
+++ Main/Aula_JavaRepeticoes.java
@@ -4,8 +4,12 @@ public class Aula_JavaRepeticoes {
     public static void main(String[] args) {
         //trabalhando com estruturas de repetições
         //FOR
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        System.out.println("Brincando de tabuada com FOR");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Tabuada do " + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(j + " x " + i + " = " + j * i);
+            }            
         }
         
     }
