a149c5c45500a052080ec9f8df384bdea75523b9
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-14 10:18:52 -0300

    aula de arrays

---
+++ Main/Aula_JavaArrays.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class Aula_JavaArrays {
+
+    public static void main(String[] args) {
+        //trabalhando com arrays
+        //no Java não podemos misturar tipos de dados dentro do array
+
+        int[] numeros = {1, 2, 3, 4, 5};
+        
+        for (int i = 0; i < numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+
+        
+    }
+}
