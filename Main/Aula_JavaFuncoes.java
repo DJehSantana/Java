b787a7b8a7156e850e7c1725412cce4776532ac3
DJehSantana <jessicah.dias23@hotmail.com>
2022-09-14 12:49:36 -0300

    realizando soma com valores passados por parametro e retornando o resultado

--- Main/Aula_JavaFuncoes.java
+++ Main/Aula_JavaFuncoes.java
@@ -9,11 +9,22 @@ public class Aula_JavaFuncoes {
        
        //para usar variáveis ao chamar um método, essas devem ser passadas por parâmetro
        saudacao(nomeOriginal);
+    
+       // a variavel resultado está recebendo o retorno da função soma
+       int resultado = soma (2, 3);
+       System.out.println("Realizando soma com funções");
+       System.out.println("Resultado: " + resultado);
+
        
        
     }
 
     public static void saudacao(String nomeParametro) {
         System.out.println("Hello World! Bem vindo: " + nomeParametro);
+    }
+
+    public static int soma (int a, int b) {
+        //retorna o resultado da operação
+        return a + b;
     }
 }
